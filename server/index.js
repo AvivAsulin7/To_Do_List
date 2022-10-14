f243fc4f1e1c0eaa94c6b4e2b6b4a004e09a377f
אביב אסולין <avivasulin1211@gmail.com>
2022-10-14 12:08:24 +0300

    changes

--- server/index.js
+++ server/index.js
@@ -15,10 +15,8 @@ app.use(cors());
 app.use("/todo", routerTodo);
 
 const PORT = process.env.PORT || 5000;
-const CONNECT =
-  "mongodb+srv://Avivasulin1211:Aviv852456@cluster0.fzixazu.mongodb.net/?retryWrites=true&w=majority";
 mongoose
-  .connect(CONNECT, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlPArser: true,
     useUnifiedTopology: true,
   })
