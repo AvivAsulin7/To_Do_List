62af277ab501835f9769c573afb3272d27a46f91
אביב אסולין <avivasulin1211@gmail.com>
2022-10-16 17:10:22 +0300

    changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/todo";
+const url = "https://to-do-list-backend-55o1.onrender.com/todo";
 
 export const fetchTodos = () => axios.get(url);
 export const createTodo = (todo) => axios.post(url, todo);
