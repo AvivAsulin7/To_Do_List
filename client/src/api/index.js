403585375115b1257a8a6c2fa17f57257c64e161
אביב אסולין <avivasulin1211@gmail.com>
2022-10-16 16:08:35 +0300

    changed URL of MongoDB

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const url = "https://to-do-list-backend-kkb8.onrender.com/todo";
+const mrl = "https://to-do-list-backend-kkb8.onrender.com/todo";
+const url = "http://localhost:3000/todo";
 
 export const fetchTodos = () => axios.get(url);
 export const createTodo = (todo) => axios.post(url, todo);
