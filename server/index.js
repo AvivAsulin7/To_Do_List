d4e9543891d8763f29f3525cd8cca2bf6246255c
אביב אסולין <avivasulin1211@gmail.com>
2022-10-18 18:53:36 +0300

    trying to fix this

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 5000;
 
-// app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use("/todo", routerTodo);
