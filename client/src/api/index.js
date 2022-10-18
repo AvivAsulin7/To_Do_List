cb63de7163ec886bad817308b7227109cd8c7bd1
אביב אסולין <avivasulin1211@gmail.com>
2022-10-18 18:52:44 +0300

    trying to solve this

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const url = "https://to-do-list-backend-55o1.onrender.com/todo";
-const url = "http://localhost:5000/todo";
+const url = "https://to-do-list-backend-fv2n.onrender.com/todo";
 
 export const fetchTodos = () => axios.get(url);
 export const createTodo = (todo) => axios.post(url, todo);
